e6793a3944da4b1b701743e859a7d9ad0b41a5d1
hansupark <soo9878@likelion.org>
2020-05-13 21:44:51 +0900

    websocket

--- WebServerTest/src/Util/MongoDBConnection.java
+++ WebServerTest/src/Util/MongoDBConnection.java
@@ -1,5 +1,7 @@
 package Util;
 
+import javax.print.Doc;
+
 import org.bson.Document;
 import org.json.simple.JSONObject;
 
@@ -8,16 +10,18 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
 public class MongoDBConnection {
 
 	private static MongoDBConnection conn = new MongoDBConnection();
 	private MongoClient mc;
     private MongoDatabase db;
     private MongoCollection<Document> collection;
+    private boolean connected; //단 한번만 connect하고 계속 사용
     
 	private MongoDBConnection() {
-		
+		connected = false;
 	}
 	
 	public static MongoDBConnection getInstance() {
@@ -29,22 +33,51 @@ public class MongoDBConnection {
 		
 		mc = MongoClients.create(); // connect to mongodb instance
     	db = mc.getDatabase("sensorDB"); // access database
-    	collection = db.getCollection("sensorData");
+    	connected = true;
 	}
 	
 	public void insertDocument(String json) {
 		
 		try {
-			connection();
+			if(!connected)
+				connection();
+			collection = db.getCollection("sensorData");
 			Document doc = Document.parse(json);
 			collection.insertOne(doc);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		finally {
-			mc.close();
+	}
+	
+	public Document findDocument_request(String MacAddress_esp32) {
+		
+		Document result = null;
+		try {
+			if(!connected)
+				connection();
+			collection = db.getCollection("request");
+			collection.find(Filters.eq("MacAddress_ESP32", MacAddress_esp32))
+					.projection(Projections.elemMatch("request")).first();
+			
+			//Mac address가 같은 document의 request만 return
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 		
+		return result;
+	}
+	public boolean closeConnection() {
+		
+		boolean result = false;
+		try {
+			mc.close();
+			result = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
--- WebServerTest/src/WebSocket/WebSocket.java
+++ WebServerTest/src/WebSocket/WebSocket.java
@@ -1,5 +1,7 @@
 package WebSocket;
 
+import java.util.HashMap;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -7,14 +9,25 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.bson.Document;
+import org.json.simple.JSONObject;
+
+import Util.JsonUtil;
 import Util.MongoDBConnection;
 
 // path : ip:8080/WebServerTest/WebSocket/WebSocket
 @ServerEndpoint("/WebSocket/WebSocket")
 public class WebSocket {
 	
-	private static MongoDBConnection conn = MongoDBConnection.getInstance();
+	private static HashMap<String,Session> Mac_SessionMapping =
+			new HashMap<String, Session>();
+	//Mac주소와 Session 맵핑
 	
+	private static HashMap<String,Integer> Mac_requestMapping = 
+			new HashMap<String,Integer>();
+	//Mac주소와 request 유무를 맵핑
+	
+	private static MongoDBConnection conn = MongoDBConnection.getInstance(); 
 	@OnOpen
 	public void handleOpen(Session session){
 		// 클라이언트가 접속시 요청되는 function
@@ -22,20 +35,66 @@ public class WebSocket {
 	}
 	
 	@OnMessage
-	public String handleMessage(String message) {
-		
+	public void handleMessage(String message,Session session) {
 		//메세지 오면 처리하는 함수
-		//json 형태로 송수신 
-//		System.out.println("from client : " + message);
-		if(message != null)
-			conn.insertDocument(message);
-		String reply = "Thank you";
-		return reply;
+		//json 형태로 송수신 		
+		
+		JSONObject jo = JsonUtil.stringToJson(message);
+		//message를 json으로 파싱
+		
+		if(!(Mac_SessionMapping.containsKey((String)jo.get("MAC")))) {
+			//맵핑 리스트에 해당 맥주소가 없다면 넣어줌
+			Mac_SessionMapping.put((String)jo.get("MAC"), session);
+		}
+		
+		String mac = "temp"; //상대방의 Mac 주소를 얻음. --> database에서 가져올 예정.
+		Session s; //상대방 세션
+		
+		if((jo.get("type")).equals("아두이노")) {
+			//아두이노에서 데이터를 보냈을때
+			if((s = Mac_SessionMapping.get(mac)) != null) {
+				//해당 세션이 맵핑이 되어있으면 상대방 세션 얻음				
+				try {
+					if(Mac_requestMapping.get(mac) == 1) {
+						s.getBasicRemote().sendObject(jo.get("sensors"));
+						Mac_requestMapping.replace(mac,0);
+					}
+					// 안드로이드에서 요청을 했을때만 보내야됨. 
+					else {
+						//데이터 베이스로 전송
+					}		
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		else {
+			//안드로이드에서 데이터를 보냈을때
+			if((s = Mac_SessionMapping.get(mac)) != null) {
+				//해당 세션이 맵핑이 되어있으면
+				String request_type = (String)jo.get("request_type");
+				String my_MAC = (String)jo.get("MAC");
+				
+				try {
+					if(request_type.equals("sensors"))
+							Mac_requestMapping.replace(my_MAC,1);
+					s.getBasicRemote().sendText((String)jo.get("request_type"));
+					// 안드로이드에서 아두이노에게 sensor값 요청 or 창문 제어 요청
+					//해당 세션으로 데이터를 보냄
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	@OnClose
 	public void handleClose() {
 		//접속 끊길때 
+		if(conn.closeConnection())
+			System.out.println("MongoDB Connection closed");
+			
 		System.out.println("Client disconnected..");
 	}
 	
@@ -44,4 +103,6 @@ public class WebSocket {
 		//Error 처리
 		t.printStackTrace();
 	}
+	
+	
 }
